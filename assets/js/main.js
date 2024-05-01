f9a748fb77c613b388451196fc60a84f4aa3278f
Abisoye Onanuga <102636953+dottymatrix@users.noreply.github.com>
2024-04-30 20:03:36 -0400

    update typing

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,7 +22,7 @@ function linkAction(){
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== TYPING EFFECT ====================*/
-const words = ['designer', 'data enthusiast', 'keyboard-enjoyer'];
+const words = ['designer','mentor', 'technical artist'];
 const typingContainer = document.querySelector('.typing-container');
 let wordIndex = 0;
 let letterIndex = 0;

90016289bf3121b7af3f022f4a64651416d05797
abisoyeonanuga <102636953+dottymatrix@users.noreply.github.com>
2024-06-18 15:22:46 -0400

    update other-section, typing JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,7 +22,7 @@ function linkAction(){
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== TYPING EFFECT ====================*/
-const words = ['designer','mentor', 'technical artist', 'learner'];
+const words = ['designer','mentor','learner'];
 const typingContainer = document.querySelector('.typing-container');
 let wordIndex = 0;
 let letterIndex = 0;
